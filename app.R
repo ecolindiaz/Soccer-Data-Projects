2b8ff1f357d1c55f03c7c70c891063a5631bd87b
Emiliano Colin-Diaz <139244097+ecolindiaz@users.noreply.github.com>
2024-04-06 16:07:20 -0400

    Update app.R

--- app.R
+++ app.R
@@ -6,15 +6,31 @@ Statsbomb <- read_excel("Recruitment Data.xlsx", sheet = "Statsbomb ")
 library(shiny)
 library(tidyverse)
 
-ui= fluidPage(
+ui = fluidPage(
+  titlePanel("Player Distances"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("yvar", "X-axis variable", 
+                  choices = c("Position", "Team")), 
+    ),  
+    mainPanel(
+      plotOutput(outputId = "distPlot"))
+    )
+  )
 
-)
 
-server = function(input, output) {
 
-   
+server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
     
+    y = "Distance"
+    
+    ggplot(Skillcorner, aes_string(x = input$xvar, y = "Goals")) +
+      geom_col()
+    
+  })
+  
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)

7f3b4e84565b38e6c2107bdcd5ff0cdb642da903
GriffenMalkin <149275954+GriffenMalkin@users.noreply.github.com>
2024-04-06 16:19:04 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -1,36 +1,39 @@
-library(readxl)
-
-Skillcorner <- read_excel("Recruitment Data.xlsx", sheet = "Skillcorner")
-Statsbomb <- read_excel("Recruitment Data.xlsx", sheet = "Statsbomb ")
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
 
 library(shiny)
 library(tidyverse)
 
+premierdivision_teams <- unique(subset(Statsbomb, Competition == "Premier Division")$Team)
+premierdivision_players <- unique(subset(Statsbomb, Competition == "Premier Division")$Name)
+
 ui = fluidPage(
-  titlePanel("Player Distances"),
+  titlePanel("Title"),
   sidebarLayout(
     sidebarPanel(
       selectInput("yvar", "X-axis variable", 
-                  choices = c("Position", "Team")), 
+                  choices = premierdivision_players), 
     ),  
     mainPanel(
-      plotOutput(outputId = "distPlot"))
-    )
+      plotOutput(outputId = "xAPlot"))
   )
-
-
+)
 
 server <- function(input, output) {
-  
   output$distPlot <- renderPlot({
     
-    y = "Distance"
+    y = "Expected Assists"
     
-    ggplot(Skillcorner, aes_string(x = input$xvar, y = "Goals")) +
+    ggplot(Skillcorner, aes_string(x = input$xvar, y = "OP xG Assisted")) +
       geom_col()
-    
   })
-  
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
